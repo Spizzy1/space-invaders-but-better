87f251d93acc88eda38b3aee4bf9b93e842c0ee1
Spizzy <spizzy47spizzy@gmail.com>
2022-12-04 20:29:15 +0100

    add stuffs

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -39,6 +40,7 @@ public class EnemyMovement : MonoBehaviour
         }
         GameObject firstX = refreshFirst();
         GameObject lastX = refreshLast();
+        GameObject lowest = refreshLowest();
         addVector = new Vector3(0.5f, 0, 0);
         bool canDo = true;
         bool allGone = false;
@@ -62,7 +64,11 @@ public class EnemyMovement : MonoBehaviour
             if(lastX == null)
             {
                 lastX = refreshLast();
-            }   
+            }
+            if(lowest == null)
+            {
+                lowest = refreshLast();
+            }
             if(lastX != null && firstX != null)
             {
                 bool goDown = false;
@@ -75,6 +81,10 @@ public class EnemyMovement : MonoBehaviour
                 {
                     addVector *= -1;
                     this.gameObject.transform.position += new Vector3(0, -0.5f, 0);
+                    if(lowest.transform.position.y - lowest.transform.localScale.y/2 < GameObject.Find("player").transform.position.y + GameObject.Find("player").transform.localScale.y/2)
+                    {
+                        SceneManager.LoadScene("GameOver");
+                    }
                 }
                 //this.gameObject.transform.position = new Vector3(Mathf.Clamp(this.gameObject.transform.position.x, (wallNegative.transform.position.x + firstX.transform.localScale.x/2) + (gameObject.transform.position.x - firstX.transform.position.x), wallPositive.transform.position.x - (lastX.transform.position.x - this.gameObject.transform.position.x)), this.gameObject.transform.position.y, 0);
 
@@ -90,22 +100,30 @@ public class EnemyMovement : MonoBehaviour
     }
     GameObject refreshFirst()
     {
-        List<GameObject> allChildrenObjects = new List<GameObject>();
-        foreach (Transform child in this.gameObject.transform)
-        {
-            allChildrenObjects.Add(child.gameObject);
-        }
+        List<GameObject> allChildrenObjects = refresh();
         GameObject firstX = allChildrenObjects.Where(x => x.transform.position.x == allChildrenObjects.Min(x => x.transform.position.x)).ToList()[0];
         return firstX;
     }
     GameObject refreshLast()
+    {
+        List<GameObject> allChildrenObjects = refresh();
+        GameObject lastX = allChildrenObjects.Where(x => x.transform.position.x == allChildrenObjects.Max(x => x.transform.position.x)).ToList()[0];
+        return lastX;
+    }
+    GameObject refreshLowest()
+    {
+        List<GameObject> allChildrenObjects = refresh();
+        GameObject lastY = allChildrenObjects.Where(x => x.transform.position.y == allChildrenObjects.Min(x => x.transform.position.y)).ToList()[0];
+        return lastY;
+
+    }
+    List<GameObject> refresh()
     {
         List<GameObject> allChildrenObjects = new List<GameObject>();
         foreach (Transform child in this.gameObject.transform)
         {
             allChildrenObjects.Add(child.gameObject);
         }
-        GameObject lastX = allChildrenObjects.Where(x => x.transform.position.x == allChildrenObjects.Max(x => x.transform.position.x)).ToList()[0];
-        return lastX;
+        return allChildrenObjects;
     }
 }
